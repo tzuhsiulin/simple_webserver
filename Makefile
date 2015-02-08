0629bc2adcacf672a8167854aa729702a52e92c1
tzuhsiulin <ltzuhsiu@gmail.com>
2015-02-08 10:42:37 +0800

    add sigchld handler

---
+++ Makefile
@@ -0,0 +1,3 @@
+
+all: 
+	gcc -D _USE_SIGCHLD_ -o server server.c
--- server.c
+++ server.c
@@ -1,3 +1,4 @@
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -10,7 +11,37 @@
 #define PORT 3000
 #define MAX 1024
 
-void handleRequestProcess(int sockfd) {
+# ifdef _USE_SIGCHLD_
+void sigchld_handler() {
+	int child_process_status;
+
+	wait(&child_process_status);
+
+	if (WIFEXITED(child_process_status)) {
+		printf("child process exited normally, and its exit code is %d\n", WEXITSTATUS(child_process_status));
+	}
+	else {
+		printf("child process exited abnormally\n");
+	}
+}
+# else
+void *zombie_process_handler(void *argu) {
+	int child_process_status;
+
+	while (1) {
+		wait(&child_process_status);
+
+		if (WIFEXITED(child_process_status)) {
+			printf("child process exited normally, and its exit code is %d\n", WEXITSTATUS(child_process_status));
+		}
+		else {
+			printf("child process exited abnormally\n");
+		}
+	}
+}
+# endif
+
+void request_handler(int sockfd) {
 	char msg[MAX + 4];
 	char *response = "Hello World";
 
@@ -28,30 +59,16 @@ void handleRequestProcess(int sockfd) {
 	close(sockfd);	
 }
 
-void *waiter(void *argu) {
-	int child_process_status;
- 
-	while (1) {
-		wait(&child_process_status);
-		if (WIFEXITED(child_process_status)) {
-			printf("child process exited normally, and its exit code is %d\n", WEXITSTATUS(child_process_status));
-		}
-		else {
-			printf("child process exited abnormally\n");
-		}
-	}
-}
-
 int main() 
 {
-	pthread_t pw_thread;
-	pid_t self, child;
+	pthread_t zph_thread;
+	pid_t self, child_pid;
 	int child_count = 0;
 	int sockfd;
 	int client_sockfd, len;
 	struct sockaddr_in server_addr;
 	struct sockaddr_in client_addr;
-	pid_t childs[MAX];
+	struct sigaction sa;
 
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
 	if (sockfd == -1) {
@@ -68,20 +85,25 @@ int main()
 		exit(1);
 	}
 
-	pthread_create(&pw_thread, NULL, &waiter, NULL);
+# ifdef _USE_SIGCHLD_
+	memset(&sa, 0, sizeof(sa));
+	sa.sa_handler = &sigchld_handler;
+	sigaction(SIGCHLD, &sa, NULL);
+# else
+	pthread_create(&zph_thread, NULL, &zombie_process_handler, NULL);
+# endif
 
 	while (1) {	
 		printf("Listening for connections...\n");
 		listen(sockfd, 5);
-		
+	
 		len = sizeof(struct sockaddr_in);
 		client_sockfd = accept(sockfd, (struct sockaddr *)&client_addr, &len);
 
-		childs[child_count] = fork();
-		if (childs[child_count] == 0) {
-			handleRequestProcess(client_sockfd);
-			child_count++;
-			return 0;
+		child_pid = fork();
+		if (child_pid == 0) {
+			request_handler(client_sockfd);
+			exit(1);
 		}
 	}
 }
